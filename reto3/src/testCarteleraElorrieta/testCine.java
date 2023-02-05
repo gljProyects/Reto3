f6986aaecda5d118cc0222e7de5510e8db69f656
gonzalo <gonzalo@DESKTOP-352FR6I>
2023-02-05 16:42:46 +0100

    Añadidos test - Cine

--- reto3/src/testCarteleraElorrieta/testCine.java
+++ reto3/src/testCarteleraElorrieta/testCine.java
@@ -1,6 +1,8 @@
 package testCarteleraElorrieta;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
 
@@ -19,17 +21,33 @@ public class testCine {
 	@Test
 	public void testToString() {
 		Cine cine = new Cine();
-		
-		cine.setCod_cine(0);
-		cine.setDireccion(null);
-		cine.setNombre(null);
-		cine.setNum_sala(0);
-		cine.setSalas(null);
 
-		String expected = "Cine [cod_cine=" + 0 + ", direccion=" + null + ", num_sala=" + 0
-				+ ", nombre=" + null + ", salas=" + null + "]";
+		String expected = "Cine [cod_cine=" + 0 + ", direccion=" + null + ", num_sala=" + 0 + ", nombre=" + null
+				+ ", salas=" + null + "]";
 		cine.toString();
 		assertEquals(expected, cine.toString());
 	}
 
+	@Test
+	public void testEqualsTrue() {
+		Cine cine = new Cine();
+		cine.setCod_cine(1);
+
+		Cine cine2 = new Cine();
+		cine2.setCod_cine(1);
+		cine.equals(cine2);
+		assertTrue(cine.equals(cine2));
+
+	}
+	@Test
+	public void testEqualsFalse() {
+		Cine cine = new Cine();
+		cine.setCod_cine(1);
+
+		Cine cine2 = new Cine();
+		cine2.setCod_cine(2);
+		cine.equals(cine2);
+		assertFalse(cine.equals(cine2));
+
+	}
 }
