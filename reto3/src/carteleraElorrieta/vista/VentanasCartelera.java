88fdcbef1f90e2ec7ec197e9289028daa231bd5d
in1dw3 <in1dw3@HZ352027.elorrieta.int>
2023-02-06 21:40:57 +0100

    Actualizacion pantallas

--- reto3/src/carteleraElorrieta/vista/VentanasCartelera.java
+++ reto3/src/carteleraElorrieta/vista/VentanasCartelera.java
@@ -1,14 +1,18 @@
 package carteleraElorrieta.vista;
 
 import java.awt.EventQueue;
+import java.awt.Image;
 
+import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import java.awt.BorderLayout;
 import javax.swing.JPanel;
 import javax.swing.JLabel;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
 public class VentanasCartelera {
-
+	private String[] imagePaths = { "img/logocine.png", "img/horror.png" };
 	private JFrame frame;
 
 	/**
@@ -39,16 +43,41 @@ public class VentanasCartelera {
 	 */
 	private void initialize() {
 		frame = new JFrame();
-		frame.setBounds(100, 100, 450, 300);
+		frame.setBounds(100, 100, 500, 250);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.getContentPane().setLayout(new BorderLayout(0, 0));
-		
-		JPanel panel = new JPanel();
-		frame.getContentPane().add(panel, BorderLayout.CENTER);
-		panel.setLayout(new BorderLayout(0, 0));
-		
+		frame.getContentPane().setLayout(null);
+
+		JPanel panelBienvenida = new JPanel();
+		panelBienvenida.setBounds(100, 100, 500, 250);
+		frame.getContentPane().add(panelBienvenida);
+		panelBienvenida.setLayout(new BorderLayout(0, 0));
+
 		JLabel lblNewLabel = new JLabel("");
-		panel.add(lblNewLabel, BorderLayout.EAST);
+		panelBienvenida.add(lblNewLabel, BorderLayout.EAST);
+		
+		
+		
+		addImage(panelBienvenida, lblNewLabel, 0);	
+		
+		panelBienvenida.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				panelBienvenida.setVisible(false);
+			}
+		});
+		
+		JPanel panelSeleccionCine = new JPanel();
+		panelSeleccionCine.setBounds(100, 100, 500, 250);
+		frame.getContentPane().add(panelSeleccionCine);
+		panelSeleccionCine.setLayout(new BorderLayout(0, 0));
+		
 	}
 
+	private void addImage(JPanel panel, JLabel label, int i) {
+		ImageIcon icon = new ImageIcon(imagePaths[i]);
+		Image img = icon.getImage();
+		Image resizedImg = img.getScaledInstance(panel.getWidth(), panel.getHeight(), Image.SCALE_SMOOTH);
+		icon.setImage(resizedImg);
+		label.setIcon(icon);
+	}
 }
