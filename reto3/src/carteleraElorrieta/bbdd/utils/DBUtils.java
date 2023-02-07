4ef96563379654d39c5d7c35a1b904f1edf97493
Gonzalo <gonzalo.barrasaal@elorrieta-errekamari.com>
2023-02-07 13:03:52 +0100

    Creacion de las primeras dos ventanas y la clase DButils

---
+++ reto3/src/carteleraElorrieta/bbdd/utils/DBUtils.java
@@ -0,0 +1,20 @@
+package carteleraElorrieta.bbdd.utils;
+
+
+	// Configuraciones necesarias para la Base de Datos de MySql
+	public class DBUtils {
+
+		// La URL donde esta la Base de Datos. Se descompone en:
+		// driver : bbd : // IP : Puerto / Schema
+		public static final String URL = "jdbc:mysql://localhost:3306/test";
+
+		// El Driver que vamos a usar
+		public static final String DRIVER = "com.mysql.cj.jdbc.Driver";
+		
+		// Nombre y Pass de acceso a la Base de Datos
+		public static final String USER = "root";
+		public static final String PASS = "";
+		
+	}
+
+
--- reto3/src/carteleraElorrieta/vista/VentanasCartelera.java
+++ reto3/src/carteleraElorrieta/vista/VentanasCartelera.java
@@ -10,9 +10,12 @@ import javax.swing.JPanel;
 import javax.swing.JLabel;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import java.awt.Font;
+import javax.swing.JComboBox;
 
 public class VentanasCartelera {
-	private String[] imagePaths = { "img/logocine.png", "img/horror.png" };
+	private String[] imagePaths = { "img/logocine.png", "img/cineBilbao.png", "img/cineMadrid.png",
+			"img/cinePamplona.png","img/imagenSalir.png" };
 	private JFrame frame;
 
 	/**
@@ -43,34 +46,102 @@ public class VentanasCartelera {
 	 */
 	private void initialize() {
 		frame = new JFrame();
-		frame.setBounds(100, 100, 500, 250);
+		frame.setBounds(450, 200, 800, 550);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().setLayout(null);
 
+		JPanel panelEleccionPelicula = new JPanel();
+		panelEleccionPelicula.setBounds(0, 0, 784, 511);
+		frame.getContentPane().add(panelEleccionPelicula);
+		panelEleccionPelicula.setLayout(null);
+		
+		JComboBox comboBox = new JComboBox();
+		comboBox.setBounds(143, 146, 432, 22);
+		panelEleccionPelicula.add(comboBox);
+		
+		
+		
+
 		JPanel panelBienvenida = new JPanel();
-		panelBienvenida.setBounds(100, 100, 500, 250);
+		panelBienvenida.setBounds(0, 0, 784, 511);
 		frame.getContentPane().add(panelBienvenida);
 		panelBienvenida.setLayout(new BorderLayout(0, 0));
 
-		JLabel lblNewLabel = new JLabel("");
-		panelBienvenida.add(lblNewLabel, BorderLayout.EAST);
+		JLabel LabelFotoBienvenida = new JLabel("");
+		panelBienvenida.add(LabelFotoBienvenida, BorderLayout.CENTER);
+		addImage(panelBienvenida, LabelFotoBienvenida, 0);
 		
+		JPanel panelSeleccionCine = new JPanel();
+		panelSeleccionCine.setBounds(0, 0, 784, 511);
+		frame.getContentPane().add(panelSeleccionCine);
+		panelSeleccionCine.setLayout(null);
+
+		JPanel panelCineBilbao = new JPanel();
+		panelCineBilbao.setBounds(20, 43, 246, 175);
+		panelSeleccionCine.add(panelCineBilbao);
+		panelCineBilbao.setLayout(new BorderLayout(0, 0));
+
+		JLabel LabelFotoCineBilbao = new JLabel("");
+		panelCineBilbao.add(LabelFotoCineBilbao, BorderLayout.CENTER);
+		addImage(panelCineBilbao, LabelFotoCineBilbao, 1);
+
+		JPanel panelCineMadrid = new JPanel();
+		panelCineMadrid.setBounds(279, 43, 264, 175);
+		panelSeleccionCine.add(panelCineMadrid);
+		panelCineMadrid.setLayout(new BorderLayout(0, 0));
+
+		JLabel LabelFotoCineMadrid = new JLabel("");
+		panelCineMadrid.add(LabelFotoCineMadrid, BorderLayout.CENTER);
+		addImage(panelCineMadrid, LabelFotoCineMadrid, 2);
+
+		JPanel panelCinePamplona = new JPanel();
+		panelCinePamplona.setBounds(553, 43, 221, 175);
+		panelSeleccionCine.add(panelCinePamplona);
+		panelCinePamplona.setLayout(new BorderLayout(0, 0));
+
+		JLabel LabelFotoCinePamplona = new JLabel("");
+		panelCinePamplona.add(LabelFotoCinePamplona, BorderLayout.CENTER);
+		addImage(panelCinePamplona, LabelFotoCinePamplona, 3);
 		
+		JLabel LabelTituloCineBilbao = new JLabel("CINE BILABO");
+		LabelTituloCineBilbao.setFont(new Font("Tahoma", Font.PLAIN, 22));
+		LabelTituloCineBilbao.setBounds(73, 229, 153, 23);
+		panelSeleccionCine.add(LabelTituloCineBilbao);
 		
-		addImage(panelBienvenida, lblNewLabel, 0);	
+		JLabel LabelTituloCineMadrid = new JLabel("CINE MADRID");
+		LabelTituloCineMadrid.setFont(new Font("Tahoma", Font.PLAIN, 22));
+		LabelTituloCineMadrid.setBounds(336, 229, 161, 23);
+		panelSeleccionCine.add(LabelTituloCineMadrid);
 		
+		JLabel LabelTituloCinePamplona = new JLabel("CINE PAMPLONA");
+		LabelTituloCinePamplona.setFont(new Font("Tahoma", Font.PLAIN, 22));
+		LabelTituloCinePamplona.setBounds(574, 229, 200, 23);
+		panelSeleccionCine.add(LabelTituloCinePamplona);
+		
+		JPanel panelSalidaSeleccionCine = new JPanel();
+		panelSalidaSeleccionCine.setBounds(304, 300, 216, 160);
+		panelSeleccionCine.add(panelSalidaSeleccionCine);
+		panelSalidaSeleccionCine.setLayout(new BorderLayout(0, 0));
+		
+		JLabel LabelFotoSalida = new JLabel("");
+		panelSalidaSeleccionCine.add(LabelFotoSalida, BorderLayout.CENTER);
+		addImage(panelSalidaSeleccionCine, LabelFotoSalida, 4);
+		
+		JLabel LabelTituloSalir = new JLabel("SALIR");
+		LabelTituloSalir.setFont(new Font("Tahoma", Font.PLAIN, 22));
+		LabelTituloSalir.setBounds(375, 466, 97, 23);
+		panelSeleccionCine.add(LabelTituloSalir);
+
 		panelBienvenida.addMouseListener(new MouseAdapter() {
 			@Override
 			public void mouseClicked(MouseEvent e) {
 				panelBienvenida.setVisible(false);
+				panelSeleccionCine.setVisible(true);
 			}
 		});
 		
-		JPanel panelSeleccionCine = new JPanel();
-		panelSeleccionCine.setBounds(100, 100, 500, 250);
-		frame.getContentPane().add(panelSeleccionCine);
-		panelSeleccionCine.setLayout(new BorderLayout(0, 0));
-		
+
+
 	}
 
 	private void addImage(JPanel panel, JLabel label, int i) {
