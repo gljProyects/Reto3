b452a88996de8e7a0df4f2b4eb5a0aa14609413a
Gonzalo <gonzalo.barrasaal@elorrieta-errekamari.com>
2023-02-14 15:27:51 +0100

    Revert "Añadida pelicula seleccionada a resumen de peliculas"
    This reverts commit a687e3cf745147a47baba3975af317d0af6bb064.

--- reto3/src/carteleraElorrieta/vista/VentanasCartelera.java
+++ reto3/src/carteleraElorrieta/vista/VentanasCartelera.java
@@ -33,7 +33,7 @@ import java.awt.Color;
 import javax.swing.UIManager;
 
 public class VentanasCartelera {
-	private ArrayList<String> emisionesConfirmadas = new ArrayList<>();
+	private ArrayList<String> emisionesConfirmadas = new ArrayList<String>();
 	private JFrame frame;
 
 	/**
@@ -68,30 +68,6 @@ public class VentanasCartelera {
 		frame.setBounds(250, 100, 800, 550);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().setLayout(null);
-
-		JPanel panelResumenCompra = new JPanel();
-		panelResumenCompra.setBackground(Color.WHITE);
-		panelResumenCompra.setBounds(0, 0, 784, 511);
-		frame.getContentPane().add(panelResumenCompra);
-		panelResumenCompra.setVisible(false);
-		panelResumenCompra.setLayout(null);
-
-		JLabel labelResumenDeCompra = new JLabel("RESUMEN DE COMPRA");
-		labelResumenDeCompra.setBounds(248, 11, 260, 39);
-		labelResumenDeCompra.setFont(new Font("Tahoma", Font.PLAIN, 22));
-		panelResumenCompra.add(labelResumenDeCompra);
-
-		JScrollPane scrollPaneResumenCompra = new JScrollPane();
-		scrollPaneResumenCompra.setBounds(10, 61, 764, 261);
-		panelResumenCompra.add(scrollPaneResumenCompra);
-
-		JTable tablaResumenCompra = new JTable();
-		scrollPaneResumenCompra.setViewportView(tablaResumenCompra);
-		Object[] columnasTablaResumenCompra = { "Horario", "Precio", "Sala", "Pelicula" };
-
-		DefaultTableModel modeloTablaResumen = new DefaultTableModel();
-		modeloTablaResumen.setColumnIdentifiers(columnasTablaResumenCompra);
-		tablaResumenCompra.setModel(modeloTablaResumen);
 
 		JPanel panelSeleccionEmision = new JPanel();
 		panelSeleccionEmision.setBackground(Color.WHITE);
@@ -178,16 +154,15 @@ public class VentanasCartelera {
 		panelBienvenida.add(LabelFotoBienvenida, BorderLayout.CENTER);
 		addImage(panelBienvenida, LabelFotoBienvenida, 0);
 
-		JButton buttonSalirEleccionCine = new JButton("Salir");
-		buttonSalirEleccionCine.addActionListener(new ActionListener() {
+		JButton ButtonSalirEleccionCine = new JButton("Salir");
+		ButtonSalirEleccionCine.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				comprobarFinalizarSesion(panelResumenCompra, panelSeleccionCine, tablaResumenCompra,
-						modeloTablaResumen, tablaEmisionesCompletas, eModel);
+				comprobarFinalizarSesion();
 			}
 
 		});
-		buttonSalirEleccionCine.setBounds(244, 223, 89, 23);
-		panelSeleccionCine.add(buttonSalirEleccionCine);
+		ButtonSalirEleccionCine.setBounds(244, 223, 89, 23);
+		panelSeleccionCine.add(ButtonSalirEleccionCine);
 
 		JButton ButtonContinuarEleccionCine = new JButton("Continuar");
 		ButtonContinuarEleccionCine.addActionListener(new ActionListener() {
@@ -259,24 +234,18 @@ public class VentanasCartelera {
 				options[0] = "Cancelar";
 				options[1] = "Confirmar";
 				String[] datosSeleccionados = new String[4];
-				volverAEleccionCine(panelEleccionPelicula, panelSeleccionCine, panelSeleccionEmision);
-
-				String fechaSeleccionada = "";
+				String fechaSeleccionada = null;
 				for (int i = 0; i < 4; i++) {
-					fechaSeleccionada = (String) eModel.getValueAt(tablaEmisionesCompletas.getSelectedRow(), i)
-							+ " " + fechaSeleccionada;
+					fechaSeleccionada = (String) eModel.getValueAt(tablaEmisionesCompletas.getSelectedRow(), i);
+					datosSeleccionados[i] = fechaSeleccionada;
 
 				}
-				emisionesConfirmadas.add(fechaSeleccionada.substring(0, fechaSeleccionada.length() - 1));
 
-			
-
-				int ret = JOptionPane.showOptionDialog(frame.getContentPane(), emisionesConfirmadas.get(0),
+				int ret = JOptionPane.showOptionDialog(frame.getContentPane(), datosSeleccionados,
 						"Esta es la emision que usted ha elegido", 0, JOptionPane.INFORMATION_MESSAGE, null, options,
 						null);
 
 				return ret;
-
 
 			}
 
@@ -287,15 +256,17 @@ public class VentanasCartelera {
 
 				if (ret == 1) {
 					volverAEleccionCine(panelEleccionPelicula, panelSeleccionCine, panelSeleccionEmision);
-					
+
+					String fechaSeleccionada = "";
 					for (int i = 0; i < 4; i++) {
-						String hola= (String) eModel.getValueAt(tablaEmisionesCompletas.getSelectedRow(), i);
-					emisionesConfirmadas.add(hola);
+						fechaSeleccionada = (String) eModel.getValueAt(tablaEmisionesCompletas.getSelectedRow(), i)
+								+ "," + fechaSeleccionada;
 
 					}
-				}
-			}
+					emisionesConfirmadas.add(fechaSeleccionada.substring(0, fechaSeleccionada.length() - 1));
 
+				} 
+			}
 		});
 
 		botonAceptarEmisionCompleta.setBounds(341, 447, 89, 23);
@@ -415,30 +386,11 @@ public class VentanasCartelera {
 
 	}
 
-	private void añadirResumenCompraTabla(JTable tablaResumenCompra, DefaultTableModel modeloTablaResumen, JTable tablaEmisionesCompletas,DefaultTableModel eModel) {
-		
-		
-		tablaResumenCompra.removeAll();
-		modeloTablaResumen.setRowCount(0);
-		if(tablaEmisionesCompletas.getSelectedRow()> -1) {
-			String hora = (String) eModel.getValueAt(tablaEmisionesCompletas.getSelectedRow(), 0);
-			String precio = (String) eModel.getValueAt(tablaEmisionesCompletas.getSelectedRow(), 1);
-			String sala = (String) eModel.getValueAt(tablaEmisionesCompletas.getSelectedRow(), 2);
-			String pelicula = (String) eModel.getValueAt(tablaEmisionesCompletas.getSelectedRow(), 3);
-			modeloTablaResumen.addRow(new String[] { hora, precio, sala, pelicula });
-		}
-
-	}
-
-	private void comprobarFinalizarSesion(JPanel panelResumenCompra, JPanel panelSeleccionCine,
-			JTable tablaResumenCompra, DefaultTableModel modeloTablaResumen, JTable tablaEmisionesCompletas, DefaultTableModel eModel) {
+	private void comprobarFinalizarSesion() {
 		if (emisionesConfirmadas.isEmpty()) {
 			frame.dispose();
 
-		} else {
-			panelSeleccionCine.setVisible(false);
-			panelResumenCompra.setVisible(true);
-			añadirResumenCompraTabla(tablaResumenCompra, modeloTablaResumen, tablaEmisionesCompletas, eModel);
 		}
 	}
+
 }
